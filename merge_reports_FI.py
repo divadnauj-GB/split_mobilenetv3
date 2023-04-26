159a17963b27054d61e262722bad9b7b350d4d54
divadnauj-GB <juan.guerrero@polito.it>
2023-04-26 13:29:18 -0700

    updated

--- merge_reports_FI.py
+++ merge_reports_FI.py
@@ -17,7 +17,10 @@ def main(args):
                 for rep_per_fault in csvfiles:
                     fault_list= pd.read_csv(os.path.join(args.path,rep_per_fault),index_col=[0]) 
                     full_report=pd.concat([full_report,fault_list],axis=0, ignore_index=True, sort=False)                
-                full_report.to_csv(os.path.join(args.path,"Misclassified_images_report.csv"))            
+                full_report.to_csv(os.path.join(args.path,"Misclassified_images_report.csv"))   
+
+                for rep_per_fault in csvfiles:
+                    os.remove(os.path.join(args.path,rep_per_fault))         
         
         fault_list_file=os.path.join(args.path,"fault_list.csv")
         fsim_report_file=os.path.join(args.path,"fsim_report.csv")
