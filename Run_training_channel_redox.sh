31249176174c077e71a17a6d44d4f351724e3f77
divadnauj-GB <juan.guerrero@polito.it>
2023-04-29 04:46:45 +0200

    added

---
+++ Run_training_channel_redox.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Function to terminate the script and all child processes
+terminate_script() {
+    echo "Terminating..."
+    # Send SIGINT signal to all child processes
+    for pid in "${child_pids[@]}"; do
+        kill -SIGINT "$pid"  # Send SIGINT signal to each child process
+    done
+    exit
+}
+
+trap 'terminate_script' INT
+
+PWD=`pwd`
+echo ${PWD}
+global_PWD="$PWD"
+echo ${CUDA_VISIBLE_DEVICES}
+
+target_config="$1"
+channels="$2"
+
+Sim_dir=$global_PWD
+mkdir -p ${Sim_dir}
+
+python ${global_PWD}/train.py\
+    --checkpoint ${global_PWD}/split${target_config}\
+    --data ${global_PWD}/data\
+    --warmup\
+    --lr 0.001\
+    --epochs 200\
+    --config ${Sim_dir}/configs/split${target_config}_ch${channels}.yaml
